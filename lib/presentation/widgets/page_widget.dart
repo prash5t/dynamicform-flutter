4f2a1217ed1d965e7fcbb191e5f44e7df606d2a4
aprashantz <prashantghi8@gmail.com>
2023-12-09 23:44:25 +0545

    update: custom text in page widget's title

--- lib/presentation/widgets/page_widget.dart
+++ lib/presentation/widgets/page_widget.dart
@@ -1,4 +1,6 @@
+import 'package:dynamicform/core/extensions/num_extensions.dart';
 import 'package:dynamicform/core/model/page_model.dart';
+import 'package:dynamicform/presentation/widgets/custom_text_widget.dart';
 import 'package:dynamicform/presentation/widgets/dynamic_field_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -12,12 +14,23 @@ class DynamicPageWidget extends StatelessWidget {
       child: SafeArea(
         child: Column(
           children: [
-            Text(pageFieldsData.pageTitle ?? "Page Title"),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: pageFieldsData.fields?.length ?? 0,
-              itemBuilder: (context, index) =>
-                  DynamicFieldWidget(fieldData: pageFieldsData.fields![index]),
+            CustomText(
+              text: pageFieldsData.pageTitle ?? "Page Title",
+              isBold: true,
+              size: 18,
+            ),
+            20.toHGap(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: pageFieldsData.fields?.length ?? 0,
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: DynamicFieldWidget(
+                      fieldData: pageFieldsData.fields![index]),
+                ),
+              ),
             )
           ],
         ),
