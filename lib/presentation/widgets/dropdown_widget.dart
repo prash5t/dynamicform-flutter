2aa1a270a2cbe8ebb308dd457dd30098fc0f6dd6
aprashantz <prashantghi8@gmail.com>
2023-12-09 23:43:29 +0545

    update: text label outside of text field widget

--- lib/presentation/widgets/dropdown_widget.dart
+++ lib/presentation/widgets/dropdown_widget.dart
@@ -1,5 +1,7 @@
+import 'package:dynamicform/core/extensions/num_extensions.dart';
 import 'package:dynamicform/core/model/field_model.dart';
 import 'package:dynamicform/core/model/option_to_select_model.dart';
+import 'package:dynamicform/presentation/widgets/custom_text_widget.dart';
 import 'package:flutter/material.dart';
 
 class DynamicDropDownWidget extends StatelessWidget {
@@ -11,9 +13,10 @@ class DynamicDropDownWidget extends StatelessWidget {
     List<DynamicOptionToSelectModel> optionsToSelect =
         fieldModel.optionsToSelect ?? [];
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(fieldModel.label ?? ""),
-        const SizedBox(height: 8),
+        CustomText(text: fieldModel.label ?? ""),
+        8.toHGap(),
         DropdownButtonFormField<String>(
             items: optionsToSelect.map((DynamicOptionToSelectModel option) {
               return DropdownMenuItem<String>(
@@ -21,6 +24,10 @@ class DynamicDropDownWidget extends StatelessWidget {
                 child: Text(option.displayValue!),
               );
             }).toList(),
+            value: optionsToSelect[0].submitValue,
+            decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(8))),
             onChanged: (pickedOption) {}),
       ],
     );
